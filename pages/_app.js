cd709f944404e028d15e1afbffbf9c2c1226a04d
sungmin-choi <namja306@naver.com>
2022-02-11 01:04:35 +0900

    Refact)사용자가 쉽게 사이트를 파악할 수 있도록 80자 이내로 설명문을수정

--- pages/_app.js
+++ pages/_app.js
@@ -16,8 +16,8 @@ function MyApp({ Component, pageProps }) {
       <link rel="canonical" href="https://o-dt.vercel.app"/>
       <meta name="viewport" content="width=device-width"/>
       <meta charSet="utf-8"/>
-      <meta name="description" content="강문해변 라인에 위치해 있으며 강문 오듯 해산물 식당
-      버터와 마늘을 볶아 만든 매콤한 미국식 해산물찜입니다 seafoodrestaurant & pub"></meta>
+      <meta name="description" content="강문해변에 위치해 있는 강문 오듯 해산물 식당
+      버터와 마늘을 볶아 만든 매콤한 미국식 해산물찜입니다"></meta>
       <meta name="keywords" content="오듯,강문오듯,강문해변,보일링씨푸드,해산물술집,강릉맛집,강문해변오듯"/>
       <meta property="og:type" content="website"/>
       <meta property="og:url" content="http://localhost:3000/"/>
@@ -25,8 +25,8 @@ function MyApp({ Component, pageProps }) {
       <meta property="og:image" content="/logo.jpg"/>
       <meta property="og:locale" content="utf-8"/>
       <meta property="og:title" content="강문 오듯 :: 강문 해산물보일링 강문 술집" />
-      <meta property="og:description" content="강문해변 라인에 위치해 있으며 강문 오듯 해산물 식당
-      버터와 마늘을 볶아 만든 매콤한 미국식 해산물찜입니다 seafoodrestaurant & pub" />
+      <meta property="og:description" content="강문해변에 위치해 있는 강문 오듯 해산물 식당
+      버터와 마늘을 볶아 만든 매콤한 미국식 해산물찜입니다" />
       <meta name="naver-site-verification" content="a9cd66601910de9531887201321e66c382df7d9d" />
       <script type="text/javascript" src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}`} ></script>
     </Head>
